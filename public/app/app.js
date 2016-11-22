7a08234c194ffcd2de47303a78c6f7a30ae7507c
rfprod <zoidenmacht@zoho.com>
2016-11-22 07:40:24 -0800

    (fix) removed compiled js, js.map files
    (update) gitignore

--- public/app/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_module_1 = require('./app.module');
-var platform = platform_browser_dynamic_1.platformBrowserDynamic();
-platform.bootstrapModule(app_module_1.AppModule)
-    .catch(function (err) {
-    console.log('platform bootstrap error:', err);
-});
-//# sourceMappingURL=app.js.map
