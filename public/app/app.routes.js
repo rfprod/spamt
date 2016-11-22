fec1e8e2c5554cea9d36b5496b79edf480fccb0d
rfprod <zoidenmacht@zoho.com>
2016-11-22 07:40:24 -0800

    (fix) removed compiled js, js.map files
    (update) gitignore

--- public/app/app.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var dashboard_intro_component_1 = require('./components/dashboard-intro.component');
-var dashboard_details_component_1 = require('./components/dashboard-details.component');
-exports.APP_ROUTES = [
-    { path: '', redirectTo: 'intro', pathMatch: 'full' },
-    { path: 'intro', component: dashboard_intro_component_1.DashboardIntroComponent },
-    { path: 'data', component: dashboard_details_component_1.DashboardDetailsComponent },
-];
-//# sourceMappingURL=app.routes.js.map
