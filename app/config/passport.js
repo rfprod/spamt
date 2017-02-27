ca545d5fd7f572dc929ec6b5e00a244bb7ca9872
rfprod <zoidenmacht@zoho.com>
2017-02-27 11:34:56 -0800

    (update) server cleanup: removed authentication strategy with email/password

--- app/config/passport.js
+++ app/config/passport.js
@@ -1,13 +1,11 @@
 'use strict';
 
-var LocalStrategy = require('passport-local').Strategy;
 var TwitterStrategy = require('passport-twitter').Strategy;
 var SoundcloudStrategy = require('passport-soundcloud').Strategy;
 var BearerStrategy = require('passport-http-api-token-bearer').Strategy;
 var User = require('../models/users');
 var configAuthTwitter = require('./auth-twitter');
 var configAuthSoundcloud = require('./auth-soundcloud');
-var crypto = require('crypto');
 
 module.exports = function(passport) {
 	passport.serializeUser((user, done) => {
@@ -79,32 +77,4 @@ module.exports = function(passport) {
 		});
 	}));
 
-	function generateDerivate(password, storedSalt) {
-		var salt, derivate, obj;
-		
-		if (storedSalt) salt = storedSalt;
-		else salt = crypto.randomBytes(24).toString('hex');
-		
-		derivate = crypto.pbkdf2Sync(password, salt, 100000, 64, 'sha512').toString('hex');
-		obj = { derivate: derivate, salt: salt };
-		//console.log(obj);
-		return obj;
-	}
-	passport.use(new LocalStrategy({
-		usernameField: 'emailLogin',
-		passwordField: 'passwordLogin',
-		passReqToCallback: true
-	}, (req, username, password, done) => {
-		process.nextTick(() => {
-			User.findOne({ 'userExtended.email': username }, (err, user) => {
-				if (err) return done(err);
-				if (!user) return done(null, false, {message: 'Unknown user'});
-				if (user.userExtended.salt) {
-					var derivateObj = generateDerivate(password, user.userExtended.salt);
-					if (user.userExtended.pass != derivateObj.derivate) return done(null, false, {message: 'Wrong password'});
-				}else if (user.userExtended.pass != password) return done(null, false, {message: 'Wrong password'});
-				return done(null, user);
-			});
-		});
-	}));
 };
