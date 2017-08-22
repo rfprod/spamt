7620a6f579250a4dc53aa6cdef0d0f111a0ae67e
rfprod <zoidenmacht@zoho.com>
2017-08-22 14:46:59 -0700

    (fix) karma test shim

--- test/karma.test-shim.js
+++ test/karma.test-shim.js
@@ -38,11 +38,13 @@ System.config({
 		'@angular/core/testing': '/base/node_modules/@angular/core/bundles/core-testing.umd.js',
 		'@angular/common/testing': '/base/node_modules/@angular/common/bundles/common-testing.umd.js',
 		'@angular/compiler/testing': '/base/node_modules/@angular/compiler/bundles/compiler-testing.umd.js',
+		'@angular/forms/testing': '/base/node_modules/@angular/forms/bundles/forms-testing.umd.js',
+		'@angular/http/testing': 'npm:@angular/http/bundles/http-testing.umd.js',
 		'@angular/platform-browser/testing': '/base/node_modules/@angular/platform-browser/bundles/platform-browser-testing.umd.js',
 		'@angular/platform-browser-dynamic/testing': '/base/node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
 		'@angular/http/testing': '/base/node_modules/@angular/http/bundles/http-testing.umd.js',
 		'@angular/router/testing': '/base/node_modules/@angular/router/bundles/router-testing.umd.js',
-		//'@angular/forms/testing': '/base/node_modules/@angular/forms/bundles/forms-testing.umd.js',
+
 
 		'systemConfig': '/base/systemjs.karma.config.js',
 		'systemConfigExtras': '/base/systemjs.config.extras.js',
