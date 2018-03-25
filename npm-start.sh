e2dd39d37006a6b1f7f413468f9fb1f39c46ab9a
rfprod <zoidenmacht@zoho.com>
2018-03-25 13:05:38 -0700

    (update) removed junky shell scripts

--- npm-start.sh
+++
@@ -1,8 +0,0 @@
-if [[ -n $OPENSHIFT_DATA_DIR ]]; then
-  echo "openshift env, data dir: $OPENSHIFT_DATA_DIR"
-  node server.js
-  #npm run production-start #custom production env
-else
-  echo "development env"
-  npm run development-start
-fi
