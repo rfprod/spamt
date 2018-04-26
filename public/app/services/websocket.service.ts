985bdb82964424a80b4bd0cb47fb5749038bbb42
rfprod <zoidenmacht@zoho.com>
2018-04-26 12:26:37 -0700

    (fix) client: websocker service

--- public/app/services/websocket.service.ts
+++ public/app/services/websocket.service.ts
@@ -9,14 +9,11 @@ export class WebsocketService {
 
 	private host: string = this.window.location.host;
 	private wsProtocol: string = (this.window.location.protocol === 'http:') ? 'ws://' : 'wss://';
-	private wsPort: string = (this.window.location.protocol === 'http:') ? '8000' : '8443';
 
-	public generateUrl(endpoint: string): string {
-		/*
-		*	generates suitable websocket url for currelty running app
-		*
-		*	8000 and 8443 are ports used by OpenShift for ws and wss connections respectively
-		*/
-		return (this.host.indexOf('localhost') !== -1) ? this.wsProtocol + this.host + endpoint : this.wsProtocol + this.host + ':' + this.wsPort + endpoint;
+	/**
+	 * Generates websocket url.
+	 */
+	public generateUrl(endpoint: string): string {		
+		return (this.host.indexOf('localhost') !== -1) ? this.wsProtocol + this.host + endpoint : this.wsProtocol + this.host + endpoint;
 	}
 }
