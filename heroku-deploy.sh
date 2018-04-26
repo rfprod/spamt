57017165bf248487fba85d407971b6617e55378b
rfprod <zoidenmacht@zoho.com>
2018-04-26 12:06:10 -0700

    (update) added concurrently, updated package script heroku-start, heroku-deploy.sh

--- heroku-deploy.sh
+++ heroku-deploy.sh
@@ -8,22 +8,19 @@ source util-echo_colours.sh
 printf "${LIGHT_GREEN} * heroku-deploy > ${GREEN}step 1:${YELLOW} checkout branch heroku-deploy${DEFAULT}\n"
 git checkout -b heroku-deploy
 
-printf "${LIGHT_GREEN} * heroku-deploy > ${GREEN}step 2:${YELLOW} stop ignoring built application with .gitignore${DEFAULT}\n"
-gulp dont-gitignore-build
+# printf "${LIGHT_GREEN} * heroku-deploy > ${GREEN}step 2:${YELLOW} stop ignoring built application with .gitignore${DEFAULT}\n"
+# gulp dont-gitignore-build
 
-printf "${LIGHT_GREEN} * heroku-deploy > ${GREEN}step 3:${YELLOW} set DEV_MODE=false in .env${DEFAULT}\n"
-gulp dont-use-cluster
+# printf "${LIGHT_GREEN} * heroku-deploy > ${GREEN}step 4:${YELLOW} build application${DEFAULT}\n"
+# gulp compile-and-build
 
-printf "${LIGHT_GREEN} * heroku-deploy > ${GREEN}step 4:${YELLOW} build application${DEFAULT}\n"
-gulp compile-and-build
+# printf "${LIGHT_GREEN} * heroku-deploy > ${GREEN}step 5:${YELLOW} add all built application files${DEFAULT}\n"
+# git add --all
 
-printf "${LIGHT_GREEN} * heroku-deploy > ${GREEN}step 5:${YELLOW} add all built application files${DEFAULT}\n"
-git add --all
+# printf "${LIGHT_GREEN} * heroku-deploy > ${GREEN}step 6:${YELLOW} commit${DEFAULT}\n"
+# git commit -m "(build) heroku build"
 
-printf "${LIGHT_GREEN} * heroku-deploy > ${GREEN}step 6:${YELLOW} commit${DEFAULT}\n"
-git commit -m "(build) heroku build"
-
-printf "${LIGHT_GREEN} * heroku-deploy > ${GREEN}step 7:${YELLOW} puh to heroku${DEFAULT}\n"
+printf "${LIGHT_GREEN} * heroku-deploy > ${GREEN}step 7:${YELLOW} push to heroku${DEFAULT}\n"
 git push heroku master --force
 
 printf "${LIGHT_GREEN} * heroku-deploy > ${GREEN}step 8:${YELLOW} checkout master${DEFAULT}\n"
